165b8e6af58566c26d52daee6314148a8c5fdebd
raquelthiele <raquelthiele@hotmail.com>
2018-08-16 17:35:26 -0300

    WireUp container, action and reducer

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return (
       <div>
-        Posts Index
+        <ul>
+        </ul>
       </div>
     );
   }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
