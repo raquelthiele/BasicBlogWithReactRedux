4a807859894f1e045dc6d4bd5ed7e8dc2da89236
raquelthiele <raquelthiele@hotmail.com>
2018-08-17 18:18:47 -0300

    Delete post from redux state

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch(action.type) {
+    case DELETE_POST:
+      return _.omit(state, action.payload); //new state without the post deleted
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     case FETCH_POST:
@@ -10,7 +12,6 @@ export default function(state = {}, action) {
       // const newState = { ...state };
       // newState[post.id] = post;
       // return newState;
-
       return { ...state, [action.payload.data.id]: action.payload.data };
     default:
       return state;
