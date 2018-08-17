288ba71e91941954da84a0d7f296775a965ebe6b
raquelthiele <raquelthiele@hotmail.com>
2018-08-17 17:05:19 -0300

    Create posts show page

---
+++ src/components/posts_show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsShow extends Component {
+  render() {
+    return (
+      <div>
+        My post here!
+      </div>
+      );
+  }
+}
+
+export default PostsShow;
